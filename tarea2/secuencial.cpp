f6246f9c01c1ea4c1b1a333b10437c4f93a32048
Cuchuflito <111323461+Cuchuflito@users.noreply.github.com>
2023-11-26 02:39:23 -0300

    Reparos secuencial
    Reparos de código secuencial

--- tarea2/secuencial.cpp
+++ tarea2/secuencial.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <chrono>
 
 using namespace cv;
 using namespace std;
+using namespace chrono;
 
 int main() {
     cout << "Loading image..." << endl;
@@ -17,25 +19,34 @@ int main() {
     }
 
     cout << "Rows (height): " << image.rows << " Cols (width): " << image.cols << endl;
-    cout << "Start conversion..." << endl;
+    cout << "Start sequential conversion..." << endl;
 
-    // Convertir la imagen a escala de grises
-    cvtColor(image, image, COLOR_BGR2GRAY);
+    // Iniciar el cronómetro
+    auto start_time = high_resolution_clock::now();
 
-    // Mostrar cada píxel en escala de grises
-    for (int r = 0; r < image.rows; r++) {
-        for (int c = 0; c < image.cols; c++) {
-            int pixel_value = image.at<uchar>(r, c);
-            cout << pixel_value << " ";
+    // Convertir la imagen a escala de grises de manera secuencial
+    Mat grayscaleImage;
+    cvtColor(image, grayscaleImage, COLOR_BGR2GRAY);
+
+    // Mostrar los primeros valores de las filas
+    cout << "First values of some rows:" << endl;
+    for (int r = 0; r < min(5, grayscaleImage.rows); r++) {
+        for (int c = 0; c < min(5, grayscaleImage.cols); c++) {
+            cout << static_cast<int>(grayscaleImage.at<uchar>(r, c)) << " ";
         }
         cout << endl;
     }
 
-    cout << "End conversion." << endl;
+    // Detener el cronómetro y calcular el tiempo transcurrido
+    auto stop_time = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop_time - start_time);
+
+    cout << "End sequential conversion." << endl;
+    cout << "Execution time: " << duration.count() << " milliseconds" << endl;
 
     // Mostrar la imagen original y la imagen en escala de grises
-    imshow("Original Image", imread("imagen.png", IMREAD_COLOR));
-    imshow("Grayscale Image", image);
+    imshow("Original Image", image);
+    imshow("Grayscale Image", grayscaleImage);
     waitKey(0);
 
     return 0;
